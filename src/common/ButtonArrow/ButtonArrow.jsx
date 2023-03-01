6b18c325a1f6db4e6164d5119a62154c7f4df98c
Kateryna Vasylieva <catarina.leon.203@gmail.com>
2023-03-01 19:11:23 +0200

    bugfix arrow btn

--- src/common/ButtonArrow/ButtonArrow.jsx
+++ src/common/ButtonArrow/ButtonArrow.jsx
@@ -3,18 +3,11 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as ArrowRight } from '../../images/SVG/icon-arrow-right.svg';
 import s from './ButtonArrow.module.css';
 
-export default function ButtonArrow({
-  className,
-  type = 'button',
-  message,
-  to,
-}) {
+export default function ButtonArrow({ className, message, to }) {
   return (
-    <button className={`${s.buttonArrow} ${className}`} type={type}>
+    <Link to={to} className={`${s.buttonArrow} ${className}`}>
       <ArrowRight className={s.buttonArrowIcon} />
-      <Link to={to} className={s.buttonArrowLink}>
-        {message}
-      </Link>
-    </button>
+      {message}
+    </Link>
   );
 }
