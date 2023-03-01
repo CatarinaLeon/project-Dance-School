c2bffb82dfbbf6c9c1760e45b1a484452da27089
Kateryna Vasylieva <catarina.leon.203@gmail.com>
2023-03-01 19:47:53 +0200

    format img

--- src/common/TitleFifthLevel/TitleFifthLevel.jsx
+++
@@ -1,5 +0,0 @@
-import s from './TitleFifthLevel.module.css';
-
-export default function TitleFifthLevel({ title, className }) {
-  return <h5 className={`${s.title} ${className}`}>{title}</h5>;
-}
--- src/common/TitleFourthLevel/TitleFourthLevel.jsx
+++
@@ -1,5 +0,0 @@
-import s from './TitleFourthLevel.module.css';
-
-export default function TitleFourthLevel({ title, className }) {
-  return <h4 className={`${s.title} ${className}`}>{title}</h4>;
-}
